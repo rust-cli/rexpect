861c684b02595547ae9bd55d9b11a640f93d81b9
Philipp Keller <philipp.keller@gmail.com>
2018-02-28 17:55:31 +0100

    show how to output the error message in case of exit code > 0

--- examples/exit_code.rs
+++ examples/exit_code.rs
@@ -8,6 +8,7 @@ use rexpect::process::wait;
 /// The following code emits:
 /// cat exited with code 0, all good!
 /// cat exited with code 1
+/// Output (stdout and stderr): cat: /this/does/not/exist: No such file or directory
 fn exit_code_fun() -> Result<()> {
 
     let p = spawn("cat /etc/passwd", Some(2000))?;
@@ -16,13 +17,18 @@ fn exit_code_fun() -> Result<()> {
         _ => println!("cat exited with code >0, or it was killed"),
     }
 
-    let p = spawn("cat /this/does/not/exist", Some(2000))?;
+    let mut p = spawn("cat /this/does/not/exist", Some(2000))?;
     match p.process.wait() {
-        Ok(wait::WaitStatus::Exited(_, c)) => println!("cat exited with code {}", c),
+        Ok(wait::WaitStatus::Exited(_, 0)) => println!("cat succeeded"),
+        Ok(wait::WaitStatus::Exited(_, c)) => {
+            println!("Cat failed with exit code {}", c);
+            println!("Output (stdout and stderr): {}", p.exp_eof()?);
+        },
         // for other possible return types of wait()
         // see here: https://tailhook.github.io/rotor/nix/sys/wait/enum.WaitStatus.html
         _ => println!("cat was probably killed"),
     }
+
     Ok(())
 }
 
