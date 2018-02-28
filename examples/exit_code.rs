9cd2a2e5417cee7d1f29985e473cee8f699921b6
Philipp Keller <philipp.keller@gmail.com>
2018-02-28 07:18:06 +0100

    add example how to handle exit code

---
+++ examples/exit_code.rs
@@ -0,0 +1,32 @@
+extern crate rexpect;
+
+use rexpect::spawn;
+use rexpect::errors::*;
+use rexpect::process::wait;
+
+
+/// The following code emits:
+/// cat exited with code 0, all good!
+/// cat exited with code 1
+fn exit_code_fun() -> Result<()> {
+
+    let p = spawn("cat /etc/passwd", Some(2000))?;
+    match p.process.wait() {
+        Ok(wait::WaitStatus::Exited(_, 0)) => println!("cat exited with code 0, all good!"),
+        _ => println!("cat exited with code >0, or it was killed"),
+    }
+
+    let p = spawn("cat /this/does/not/exist", Some(2000))?;
+    match p.process.wait() {
+        Ok(wait::WaitStatus::Exited(_, c)) => println!("cat exited with code {}", c),
+        // for other possible return types of wait()
+        // see here: https://tailhook.github.io/rotor/nix/sys/wait/enum.WaitStatus.html
+        _ => println!("cat was probably killed"),
+    }
+    Ok(())
+}
+
+
+fn main() {
+    exit_code_fun().unwrap_or_else(|e| panic!("cat function failed with {}", e));
+}
